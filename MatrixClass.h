23872cdf0c282784fe38969ce20f0d37c2b075d6
Executron <aniket.2211994@gmail.com>
2015-10-20 13:01:13 +0530

    adding multiply

--- MatrixClass.h
+++ MatrixClass.h
@@ -66,15 +66,7 @@ struct vMatrix
 		}
 	}
 
-	vMatrix<R, C> operator+(vMatrix<R, C>& A)
-	{
-		vMatrix<R,C> Result;
-		for (int i = 0; i < A.Elements.size(); i++)
-		{
-			Result.Elements[i] = this->Elements[i] + A.Elements[i];
-		}
-		return Result;
-	}
+	//ASSIGNMENT
 
 	vMatrix<R, C> operator=(vMatrix<R, C>&A)
 	{
@@ -85,4 +77,97 @@ struct vMatrix
 		}
 		return Result;
 	}
+
+	//ADDITION
+
+	vMatrix<R, C> operator+(vMatrix<R, C>& A)
+	{
+		vMatrix<R,C> Result;
+		for (int i = 0; i < A.Elements.size(); i++)
+		{
+			Result.Elements[i] = this->Elements[i] + A.Elements[i];
+		}
+		return Result;
+	}
+
+	vMatrix<R, C> operator+=(vMatrix<R, C>& A)
+	{
+		return this + A;
+	}
+
+	//SUBSTRACTION
+
+	vMatrix<R, C> operator-(vMatrix<R, C>& A)
+	{
+		vMatrix<R, C> Result;
+		for (int i = 0; i < A.Elements.size(); i++)
+		{
+			Result.Elements[i] = this->Elements[i] - A.Elements[i];
+		}
+		return Result;
+	}
+
+	vMatrix<R, C> operator-=(vMatrix<R, C>& A)
+	{
+		return this - A;
+	}
+
+	//MULTIPLY
+
+	template < int C2 >
+	vMatrix<R, C2> operator*(vMatrix<C, C2>& other) //mXn * pXq ... n==p . size = mXq
+	{
+		vMatrix<R, C2> Result;
+		cout << "Matrix 1: " << R << "X" << C << ". Matrix 2: " << C << "X" << C2 << std::endl;
+		for (int i = 0; i < R; i++)
+		{
+			for (int j = 0; j < C2; j++)
+			{
+				float total = 0;
+				for (int k = 0; k < C; k++)
+				{
+					total += (this->Elements[i * C + k] * other.Elements[k * C2 + j]);
+				}
+				Result.Elements[i*C2 + j] = total;
+			}
+		}
+		return Result;
+	}
+
+	template < int C2 > 
+	vMatrix<R, C2> operator*=(vMatrix<C, C2>& other) //mXn * pXq ... n==p . size = mXq
+	{
+		return this*other;
+	}
+
+	vMatrix<R, C> operator*(float scale)
+	{
+		vMatrix<R, C> Result;
+		for (int i = 0; i < Elements.size();i++)
+		{
+			Result.Elements[i] = scale*Elements[i];
+		}
+	}
+
+	//DETERMINATE
+	template < int D >
+	float det(vMatrix<D, D> &mat)
+	{
+		//OMG SO COMPLEX AND SO RECURSIVE!
+	}
+
+	//Transpose
+	vMatrix<C, R> transpose()
+	{
+		vMatrix<C, R> Result;
+		for (int i = 0; i < C; i++)
+		{
+			for (int j = 0; j < R; j++)
+			{
+				Result.Elements[i*R + j] = Elements[j*C + i];
+			}
+		}
+		return Result;
+	}
+
 };
